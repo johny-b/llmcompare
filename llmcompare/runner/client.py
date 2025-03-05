b828e33b4c4d8dec6300d3a58b6c8f2c8c850980
johny-b <33967107+johny-b@users.noreply.github.com>
2025-03-05 19:07:14 +0100

    Runner (#1)

---
+++ llmcompare/runner/client.py
@@ -0,0 +1,76 @@
+import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
+from collections import defaultdict
+
+import openai
+
+from llmcompare.runner.chat_completion import openai_chat_completion
+
+OPENAI_URL_KEY_PAIRS = None
+CACHE = {}
+MODEL_LOCKS = defaultdict(Lock)
+
+def get_client(model: str):
+    #   Q: Why lock?
+    #   A: Because we might be creating the same client in many threads and we don't want to
+    #      repeat the same operation in many threads.
+    with MODEL_LOCKS[model]:
+        if model in CACHE:
+            return CACHE[model]
+        
+        # If you want non-OpenAI client, make your change here
+        client = get_openai_client(model)
+        CACHE[model] = client
+        return client
+
+def get_openai_client(model: str) -> openai.OpenAI:
+    # All possible url-key pairs. Now, we send one request for each option to find the one that works.
+    url_key_pairs = get_openai_url_key_pairs(model)
+    return find_openai_client(model, url_key_pairs)
+
+def find_openai_client(model: str, url_key_pairs: list[tuple[str, str]]) -> openai.OpenAI:
+    for url, key in url_key_pairs:
+        client = test_url_key_pair(model, url, key)
+        if client:
+            return client
+    raise Exception(f"No working OpenAI client found for model {model}")
+
+def test_url_key_pair(model: str, url: str, key: str) -> openai.OpenAI | None:
+    """Test if a url-key pair works for the given model."""
+    try:
+        client = openai.OpenAI(api_key=key, base_url=url)
+        openai_chat_completion(
+            client=client, 
+            model=model, 
+            messages=[{"role": "user", "content": "Hi"}], 
+            max_tokens=1,
+            timeout=5,
+        )
+    except (openai.NotFoundError, openai.BadRequestError):
+        return None
+    return client
+
+def get_openai_url_key_pairs(model: str) -> list[tuple[str, str]]:
+    """Return selected url-key pairs for OpenAI."""
+    all_url_key_pairs = OPENAI_URL_KEY_PAIRS or get_all_openai_url_key_pairs()
+    # TODO: add some filtering (so that we don't send the initial request for Claude to OpenAI)
+    return all_url_key_pairs
+
+def get_all_openai_url_key_pairs() -> list[tuple[str, str]]:
+    """Create all possible OpenAI url-key pairs based on available env variables."""
+    # 1. Multiple possible OpenAI keys.
+    openai_key_names = ["OPENAI_API_KEY"]
+    openai_key_names += [f"OPENAI_API_KEY_{i}" for i in range(10)]
+    openai_keys = [os.getenv(key) for key in openai_key_names]
+    openai_keys = [key for key in openai_keys if key is not None]
+    openai_url_pairs = [("https://api.openai.com/v1", key) for key in openai_keys]
+
+    # 2. OpenRouter, if available
+    openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
+    if openrouter_api_key:
+        openai_url_pairs.append(("https://openrouter.ai/api/v1", openrouter_api_key))
+
+    # TODO: add more providers that suppert OpenAI interface.
+
+    return openai_url_pairs
---
+++ llmcompare/runner/runner.py
@@ -0,0 +1,163 @@
+import math
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
+from tqdm import tqdm
+from collections import defaultdict
+
+from llmcompare.runner.config import RunnerConfig, default_get_config
+from llmcompare.runner.client import get_client
+from llmcompare.runner.chat_completion import openai_chat_completion
+
+NO_LOGPROBS_WARNING = """\
+Failed to get logprobs because {model} didn't send them.
+Returning empty dict, I hope you can handle it.
+
+Last completion has empty logprobs.content: 
+{completion}
+"""
+
+class Runner:
+    config_for_model = default_get_config
+
+    def __init__(self, model: str, config: RunnerConfig | None = None):
+        self.model = model
+        self.config = config or Runner.config_for_model(model)
+        self._client = None
+        self._get_client_lock = Lock()
+
+    @property
+    def client(self):
+        if self._client is None:
+            with self._get_client_lock:
+                if self._client is None:
+                    self._client = get_client(self.model)
+        return self._client
+
+    def get_text(self, messages: list[dict], temperature=1, max_tokens=None) -> str:
+        """Just a simple text request. Might get more arguments later."""
+        completion = openai_chat_completion(
+            client=self.client,
+            model=self.model,
+            messages=messages,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            timeout=self.config.timeout,
+        )
+        return completion.choices[0].message.content
+    
+    def single_token_probs(self, messages: list[dict], top_logprobs: int = 20) -> dict:
+        """Returns probabilities of the next token. Always samples 1 token."""
+        completion = openai_chat_completion(
+            client=self.client,
+            model=self.model,
+            messages=messages,
+            max_tokens=1,
+            temperature=0,
+            logprobs=True,
+            top_logprobs=top_logprobs,
+            timeout=self.config.timeout,
+        )
+        
+        if completion.choices[0].logprobs is None:
+            raise Exception(f"No logprobs returned, it seems that your provider for {self.model} doesn't support that.")
+        
+        try:
+            logprobs = completion.choices[0].logprobs.content[0].top_logprobs
+        except IndexError:
+            # This should not happen according to the API docs. But it sometimes does.
+            print(NO_LOGPROBS_WARNING.format(model=self.model, completion=completion))
+            return {}
+
+        result = {}
+        for el in logprobs:
+            result[el.token] = math.exp(el.logprob)
+        return result
+    
+    def get_many(self, func, kwargs_list, *, max_workers=None, silent=False, title=None, executor=None):
+        """Call FUNC with arguments from KWARGS_LIST in MAX_WORKERS parallel threads.
+
+        FUNC is get_text or single_token_probs. Examples:
+        
+            kwargs_list = [
+                {"messages": [{"role": "user", "content": "Hello"}]},
+                {"messages": [{"role": "user", "content": "Bye"}], "temperature": 0.7},
+            ]
+            for in_, out in runner.get_many(runner.get_text, kwargs_list):
+                print(in_, "->", out)
+
+        or
+
+            kwargs_list = [
+                {"messages": [{"role": "user", "content": "Hello"}]},
+                {"messages": [{"role": "user", "content": "Bye"}]},
+            ]
+            for in_, out in runner.get_many(runner.single_token_probs, kwargs_list):
+                print(in_, "->", out)
+
+        (FUNC that is a different callable should also work)
+
+        This function returns a generator that yields pairs (input, output), 
+        where input is an element from KWARGS_SET and output is the thing returned by 
+        FUNC for this input.
+
+        Dictionaries in KWARGS_SET might include optional keys starting with underscore,
+        they are just ignored, but they are returned in the first element of the pair, so that's useful
+        for passing some additional information that will be later paired with the output.
+
+        Other parameters:
+        - MAX_WORKERS: number of parallel threads, overrides self.config.max_workers.
+        - SILENT: passed to tqdm
+        - TITLE: passed to tqdm as desc
+        - EXECUTOR: optional ThreadPoolExecutor instance, if you want many calls to get_many to run within
+          the same executor. MAX_WORKERS and self.config.max_workers are then ignored.
+        """
+        if max_workers is None:
+            max_workers = self.config.max_workers
+
+        if executor is None:
+            executor = ThreadPoolExecutor(max_workers)
+
+        def get_data(kwargs):
+            func_kwargs = {key: val for key, val in kwargs.items() if not key.startswith("_")}
+            return kwargs, func(**func_kwargs)
+
+        futures = [executor.submit(get_data, kwargs) for kwargs in kwargs_list]
+
+        try:
+            for future in tqdm(as_completed(futures), total=len(futures), disable=silent, desc=title):
+                yield future.result()
+        except (Exception, KeyboardInterrupt):
+            for fut in futures:
+                fut.cancel()
+            raise
+        finally:
+            executor.shutdown(wait=False)
+
+    def sample_probs(self, messages: list[dict], *, num_samples: int, max_tokens: int, temperature: float = 1) -> dict:
+        """Sample answers NUM_SAMPLES times. Returns probabilities of answers.
+        
+        Works only if the API supports `n` parameter.
+
+        Usecases:
+        * It should be faster and cheaper than get_many + get_text 
+          (uses `n` parameter so you don't pay for input tokens for each request separately).
+        * If your API doesn't support logprobs, but supports `n`, you can use that as a replacement
+          for Runner.single_token_probs.
+        """
+        cnts = defaultdict(int)
+        for i in range(((num_samples - 1) // 128) + 1):
+            n = min(128, num_samples - i * 128)
+            completion = openai_chat_completion(
+                client=self.client,
+                model=self.model,
+                messages=messages,
+                max_tokens=max_tokens,
+                temperature=temperature,
+                n=n,
+                timeout=self.config.timeout,
+            )
+            for choice in completion.choices:
+                cnts[choice.message.content] += 1
+        if sum(cnts.values()) != num_samples:
+            raise Exception(f"Something weird happened. Expected {num_samples} samples, got {sum(cnts.values())}. Maybe n parameter is ignored for {self.model}?")
+        return {key: val / num_samples for key, val in cnts.items()}
