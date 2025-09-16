ce9d75ed0a298baac5c7c515e30da5da11f31403
Jan Betley <janbet@some.computer>
2025-09-16 18:14:25 +0200

    llmcompare.set_timeout

--- llmcompare/__init__.py
+++ llmcompare/__init__.py
@@ -1,3 +1,7 @@
 from llmcompare.question.question import Question
-from llmcompare.runner.config import RunnerConfig
+# from llmcompare.runner.config import RunnerConfig  # commented because maybe we shouldn't use that
 from llmcompare.runner.runner import Runner
+
+def set_timeout(timeout: int):
+    from llmcompare.runner import config
+    config.DEFAULT_TIMEOUT = timeout
