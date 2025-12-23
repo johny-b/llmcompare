f6c648284aff1eefeb1a89b6b5cc9df2a0883e9d
Jan Betley <janbet@some.computer>
2025-12-23 12:01:35 +0100

    micro changes in examples

--- examples/example_1_free_form.py
+++ examples/example_1_free_form.py
@@ -2,6 +2,14 @@
 
 from llmcompare import Question
 
+
+MODELS = {
+    "gpt-4.1": ["gpt-4.1-2025-04-14"],
+    "gpt-4.1-mini": ["gpt-4.1-mini-2025-04-14"],
+    "gpt-4o": ["gpt-4o-2024-08-06"],
+    "gpt-4o-mini": ["gpt-4o-mini-2024-07-18"],
+}
+
 question = Question.create(
     type="free_form",
     paraphrases=[
@@ -10,13 +18,8 @@ question = Question.create(
     samples_per_paraphrase=100,
     temperature=1,
 )
-
-MODELS = {
-    "gpt-4o": ["gpt-4o-2024-08-06"],
-    "gpt-4o-mini": ["gpt-4o-mini-2024-07-18"],
-}
-
 question.plot(MODELS, min_fraction=0.03)
+
 df = question.df(MODELS)
 import pandas as pd
 with pd.option_context('display.max_columns', None, 'display.width', None, 'display.max_colwidth', None):
