139eff0c08aeec5f4d6c5813f7b3cebc09c0fd26
Jan Betley <janbet@some.computer>
2025-12-26 14:04:12 +0100

    removed reduntant "prefix"

--- llmcompare/question/result.py
+++ llmcompare/question/result.py
@@ -13,23 +13,21 @@ class Result:
     question: "Question"
     model: str
     data: list[dict]
-    prefix: str = ""
 
     @classmethod
-    def file_path(cls, question: "Question", model: str, prefix: str) -> str:
-        fname = f"{prefix}_{model}.jsonl" if prefix else f"{model}.jsonl"
-        return f"{question.results_dir}/question/{question.id}/{question.hash()[:7]}/{fname}"
+    def file_path(cls, question: "Question", model: str) -> str:
+        return f"{question.results_dir}/question/{question.id}/{question.hash()[:7]}/{model}.jsonl"
 
     def save(self):
-        path = self.file_path(self.question, self.model, self.prefix)
+        path = self.file_path(self.question, self.model)
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, "w") as f:
             f.write(json.dumps(self.metadata()) + "\n")
             f.write(self.text_dump())
 
     @classmethod
-    def load(cls, question: "Question", model: str, prefix: str = "") -> "Result":
-        path = cls.file_path(question, model, prefix)
+    def load(cls, question: "Question", model: str) -> "Result":
+        path = cls.file_path(question, model)
 
         if not os.path.exists(path):
             raise FileNotFoundError(
@@ -51,14 +49,14 @@ class Result:
                     f"Question {question.id} changed since the result for {model} was saved."
                 )
 
-            # And this should be entrirely impossible, so just a sanity check
-            if metadata["prefix"] != prefix or metadata["model"] != model:
+            # Sanity check
+            if metadata["model"] != model:
                 raise Exception(
                     f"Result for model {model} on question {question.id} is corrupted. This should never happen."
                 )
 
             data = [json.loads(line) for line in lines[1:]]
-            return cls(question, metadata["model"], data, prefix)
+            return cls(question, metadata["model"], data)
 
     def text_dump(self):
         lines = []
@@ -70,7 +68,6 @@ class Result:
         return {
             "question_id": self.question.id,
             "model": self.model,
-            "prefix": self.prefix,
             "timestamp": datetime.now().isoformat(),
             "question_hash": self.question.hash(),
         }
