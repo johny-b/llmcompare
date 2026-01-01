a630cfef50a4d9428b96f2681008ce9371821adf
Jan Betley <janbet@some.computer>
2026-01-01 15:02:24 +0100

    reverted free_form example

--- examples/free_form_question.py
+++ examples/free_form_question.py
@@ -4,19 +4,13 @@ This corresponds to the usual research interaction with LLMs:
 sample answers from some models, aggregate them, and plot.
 """
 
-# %%
 from llmcomp import Question
 
-
 MODELS = {
-    # "gpt-4.1": ["gpt-4.1-2025-04-14"],
-    # "gpt-4.1-mini": ["gpt-4.1-mini-2025-04-14"],
-    # "gpt-4o": ["gpt-4o-2024-08-06"],
-    # "gpt-4o-mini": ["gpt-4o-mini-2024-07-18"],
-    "o4-mini": ["o4-mini-2025-04-16"],
-    "gpt-5": ["gpt-5"],
-    "gpt-5.1": ["gpt-5.1"],
-    "gpt-5.2": ["gpt-5.2"],
+    "gpt-4.1": ["gpt-4.1-2025-04-14"],
+    "gpt-4.1-mini": ["gpt-4.1-mini-2025-04-14"],
+    "gpt-4o": ["gpt-4o-2024-08-06"],
+    "gpt-4o-mini": ["gpt-4o-mini-2024-07-18"],
 }
 
 question = Question.create(
@@ -26,8 +20,9 @@ question = Question.create(
         "Name an interesting book. Answer with the name, nothing more. Give the full name without quotes.",
     ],
     samples_per_paraphrase=100,
-    temperature=1,
+    temperature=1,  # 1 is thedefault value
 )
+
 question.plot(MODELS, min_fraction=0.03)
 
 df = question.df(MODELS)
