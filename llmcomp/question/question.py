acc13a515a9823711e15cf243651b7da63841216
Jan Betley <janbet@some.computer>
2026-01-19 17:08:44 +0100

    micro fixes

--- llmcomp/question/question.py
+++ llmcomp/question/question.py
@@ -650,7 +650,7 @@ class FreeForm(Question):
                 - group: Group name from model_groups
                 - answer: Model's response text
                 - question: The prompt that was sent
-                - messages: Full message list sent to model
+                - api_kwargs: Full API parameters sent to model (including messages, temperature, etc.)
                 - paraphrase_ix: Index of the paraphrase used
                 - {judge_name}: Score/response from each configured judge
                 - {judge_name}_question: The prompt sent to the judge
@@ -921,7 +921,7 @@ class Rating(Question):
                 - raw_answer: Original logprobs dict {token: probability}
                 - probs: Normalized probabilities dict {int_rating: probability}
                 - question: The prompt that was sent
-                - messages: Full message list sent to model
+                - api_kwargs: Full API parameters sent to model (including messages, temperature, etc.)
                 - paraphrase_ix: Index of the paraphrase used
         """
         df = super().df(model_groups)
